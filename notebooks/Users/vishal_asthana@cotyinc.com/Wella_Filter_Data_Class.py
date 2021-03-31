d63d02db0349f241ee9f4e9a43130245ec2463c0
vishal189asthana <vishal.asthana@brillio.com>
2021-03-31 14:16:29 +0530

    added main function

--- notebooks/Users/vishal_asthana@cotyinc.com/Wella_Filter_Data_Class.py
+++ notebooks/Users/vishal_asthana@cotyinc.com/Wella_Filter_Data_Class.py
@@ -13,7 +13,18 @@
 
 # DBTITLE 1,connection string for ADLS and SPARK
 
+from time import time
+from datetime import timedelta
+from pyspark.sql.functions import *
 
+
+class T():
+    def __enter__(self):
+        self.start = time()
+    def __exit__(self, type, value, traceback):
+        self.end = time()
+        elapsed = self.end - self.start
+        print(str(timedelta(seconds=elapsed)))
 #Establish the connection between ADLS and Databricks
 #spark.conf.set("fs.azure.account.key.<your-storage-account-name>.dfs.core.windows.net","<access-key>")
 
@@ -24,59 +35,57 @@ spark.conf.set("fs.azure.account.key.cashanalysislake.dfs.core.windows.net",dbut
 
 # DBTITLE 1,File schema properties 
 class load_ADL_raw:
+    
     def __init__(self, file_type,infer_schema, first_row_is_header,delimiter,filelocation):
         self.file_type=file_type
         self.infer_schema = infer_schema
         self.first_row_is_header = first_row_is_header
         self.delimiter=delimiter
         self.filelocation=filelocation
-
+    
     def load_schema_ADL(self):
         print(self.file_type,self.infer_schema, self.first_row_is_header,self.delimiter,self.filelocation)
 
 # COMMAND ----------
 
-# DBTITLE 1,Main function for reading the data frame from ADLS
-if __name__ == "__main__":
 
-    x=load_ADL_raw("csv","false","true","ψ","abfss://archive@cashanalysislake.dfs.core.windows.net/Finance/AccountsPayables/Daily/Current/Processed/201912/*.csv")
-
+def load_adl_raw_data():
+    x = load_ADL_raw("csv", "false", "true", "ψ",
+                     "abfss://archive@cashanalysislake.dfs.core.windows.net/Finance/AccountsPayables/Daily/Current/Processed/201912/*.csv")
     df = spark.read.format(x.file_type) \
         .option("inferSchema", x.infer_schema) \
         .option("header", x.first_row_is_header) \
         .option("sep", x.delimiter) \
         .option("encoding", "utf-8") \
         .load(x.filelocation)
-    
-    display(df)
-
-
-
-
-# COMMAND ----------
-
-# DBTITLE 1,Adding flag and identifier in the dataframe
-from pyspark.sql.functions import *
-df=df.withColumn("file_name", input_file_name())
-df.dropDuplicates(['file_name'])
-df.createOrReplaceTempView("temp_table")
-new_df = spark.sql("select count(*) as rows_count, file_name from temp_table group by file_name")
-
-# COMMAND ----------
-
-logger.info("count  is  %s",new_df.show(20, False))
-
-# COMMAND ----------
-
+    return df
 
 
 # COMMAND ----------
 
 
+def load_main_function():
+    with T():
+        df = load_adl_raw_data()
+
+        df = df.withColumn("file_name", input_file_name())
+        df.dropDuplicates(['file_name'])
+        df.createOrReplaceTempView("temp_table")
+        new_df = spark.sql("select count(*) as rows_count, file_name from temp_table group by file_name")
+        new_df.show(20,False)
+        logger.info("count  is  %s",new_df.show(20, False))
 
 # COMMAND ----------
 
-df.show(2,False)
+# DBTITLE 1,Main function for reading the data frame from ADLS
+if __name__ == "__main__":
+  load_main_function()
+  
+
+	
+
+
+
 
 # COMMAND ----------
 
